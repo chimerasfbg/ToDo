8214a194cb58810e0cdcd64920b0a732d8df1198
chimera <chimerasf.bg@gmail.com>
2021-11-23 23:17:36 +0100

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('form');
+const addBtn = document.querySelector('.todo-btn');
+const displayContainer = document.querySelector('.todo-container');
+const ul = document.querySelector('.todo-list');
+
+
+form.addEventListener('click', ()=>{
+
+});
