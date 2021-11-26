06dcaa17a649595bba06dabfdca4a35eb210a32a
chimera <chimerasf.bg@gmail.com>
2021-11-26 16:41:23 +0100

    Styles added

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-const form = document.querySelector('form');
+const form = document.querySelector('.todo-input');
 const addBtn = document.querySelector('.todo-btn');
 const displayContainer = document.querySelector('.todo-container');
 const ul = document.querySelector('.todo-list');
+const file =document.querySelector('.container');
 
-
-form.addEventListener('click', ()=>{
-
+addBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let input = form.value.trim();
+    ul.innerHTML += `
+    <div class="mb-3 mt-1 d-flex justify-content-center align-items-center ">
+    <li class="mt-1 form-control item">${input}
+        <div class="float-end">
+            <button class="btn-primary remove" ><i class=" add fas fa-trash" ></i></button>
+            <button class="btn-primary check"><i class=" add fas fa-check"></i></button>
+        </div>  
+    </li>
+    
+    </div>`;
+    
+    form.value=""
 });
+
+displayContainer.addEventListener('click',(e)=>{
+    e.preventDefault();
+    if(e.target.classList.contains('remove')){
+        e.target.parentElement.parentElement.parentElement.remove()
+        
+    } 
+    if (e.target.classList.contains('check')){
+        e.target.parentElement.parentElement.classList.toggle('check');
+        setTimeout(function(){
+            e.target.parentElement.parentElement.parentElement.remove()
+        },4000)
+    }
+})
